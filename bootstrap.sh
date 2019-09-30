e0823ff189e5966b2a9b0f6ceb391b636219d2c6
makdoudN <nizam.makdoud@gmail.com>
2019-09-30 16:12:52 +0200

    x

---
+++ bootstrap.sh
@@ -0,0 +1,17 @@
+
+
+
+# Setup: Base 
+
+apt update && apt upgrade
+apt install curl wget
+
+
+# Setup: Rust
+
+curl https://sh.rustup.rs -sSf | sh
+
+# CLI Tools
+
+cargo install exa            # https://github.com/ogham/exa
+
